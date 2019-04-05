b7c7438fc6e0c0a44af607ad5527474f8cd50fb5
Jacson Passold <jacson@passold.com.br>
2019-04-05 15:27:21 -0300

    check if is a post or page with content before exec js script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,18 @@
 jQuery(document).ready(function() {
-    jQuery('.site-content').fullpage({
-    	// Navigation
-    	anchors:['intro', 'para-quem-fazemos', 'o-que-fazemos', 'como-fazemos', 'cadastro'],
-
-    	// Design
-    	fixedElements: '.site-footer',
-
-    	// Scroll
-    	autoScrolling: false,
-    	fitToSection: false,
-
-    	// Custom selectors
-        sectionSelector: '.pane',
-    });
+    if(jQuery('.site-content').lenght > 0) {
+        jQuery('.site-content').fullpage({
+        	// Navigation
+        	anchors:['intro', 'para-quem-fazemos', 'o-que-fazemos', 'como-fazemos', 'cadastro'],
+    
+        	// Design
+        	fixedElements: '.site-footer',
+    
+        	// Scroll
+        	autoScrolling: false,
+        	fitToSection: false,
+    
+        	// Custom selectors
+            sectionSelector: '.pane',
+        });
+    }
 });
